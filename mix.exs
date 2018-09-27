9b335c1d0b2482335e464ab5c143615f39748909
zwetsloot robert jan <zwetsloot.robert.jan@techcross.co.jp>
2018-09-27 14:51:08 +0900

    Added exrm as dependency for releasing

--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule ChessPlus.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:exrm, "~> 1.0.8"},
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.16", only: [:dev], runtime: false},
       {:map_diff, "~> 1.0", only: [:dev, :test]},
